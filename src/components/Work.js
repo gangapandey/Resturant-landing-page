a4e7f3d6ea8637ba73bcc0a325f4e9ba37ec0bae
gangapandey <igangapandey@gmail.com>
2023-11-08 12:10:52 +0545

    work-section

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import PickMeals from'../assets/pick-meals-image.png'
+import ChooseMeals from '../assets/choose-image.png'
+import DeliveryMeals from '../assets/delivery-image.png'
 
 const Work = () => {
+  const workInfoData = [
+    {
+      image: PickMeals,
+      title: "Pick Meals",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis loreum."
+    },
+    {
+      image: ChooseMeals,
+      title: "Choose How Often",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et Maecenas orci etLorem"
+    },
+    {
+      image: DeliveryMeals,
+      title: "Fast Deliveries",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci etc Lorem ipsum loreum "
+    },
+  ]
+
   return (
-    <div>
+    <div className='work-section-wrapper'>
+
+      <div className='work-section-top'>
+        <p className='primary-subheading'>Work</p>
+        <h1 className='primary-heading'>How It Works</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+
+      <div className='work-section-box'>
+        {workInfoData.map((data) => (
+          <div className='work-section-info' key={data.title}>
+
+            <div className='info-boxes-img-container'>
+              <img src= {data.image} alt = "img" />
+            </div>
+
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+      </div>
       
     </div>
   )
